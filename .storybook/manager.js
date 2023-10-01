3c9a39f41eb6331a94255e7e13a8857d2badf417
tarashagarwal <tarash.agarwal@gmail.com>
2023-10-01 10:06:45 -0300

    Initial Commit

---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from '@storybook/addons';
+
+addons.setConfig({
+    // This will disable the new story store (storyStoreV7) introduced in Storybook 7.
+    storyStoreV7: true,
+});
